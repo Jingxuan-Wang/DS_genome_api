b3af4d0c6365858c8dc642e9ccb1a07278ca054f
Jingxuan-Wang <jingxuan@singtel.com>
2020-03-06 13:07:46 +1100

    change elements in basic_query to private

--- api/basic_query.py
+++ api/basic_query.py
@@ -17,72 +17,71 @@ class BasicQuery:
   def __init__(self, end_point:str, token:str = ""):
     self.query_path = "/".join([self._URLS, end_point, self._API_ENDPOINT[end_point], 'query'])
     self.token = token
-    self.dt = None
-    self.aggs = None
-    self.ts_reference = None
-    self.d_facets = None
-    self.grant = None
-    self.loc = None
-    self.filt = None
+    self._dt = None
+    self._aggs = None
+    self._ts_reference = None
+    self._d_facets = None
+    self._grant = None
+    self._loc = None
+    self._filt = None
     self.req = {}
 
   def dates(self, begin_date: str, end_date: str = None):
     dt = Dates()
-    self.dt = dt(begin_date, end_date=end_date)
+    self._dt = dt(begin_date, end_date=end_date)
     return self
 
   def aggregate(self, metric: str, typ: str, described_as=None):
     agg = Aggregation()
-    if self.aggs is None:
-      self.aggs = agg(metric=metric, typ=typ, described_as=described_as) ## assign self.aggs as Aggregations Object
+    if self._aggs is None:
+      self._aggs = agg(metric=metric, typ=typ, described_as=described_as) ## assign self.aggs as Aggregations Object
     else:
-      self.aggs += agg(metric=metric, typ=typ, described_as=described_as) ## adding other Aggregations Object to self.aggs
+      self._aggs += agg(metric=metric, typ=typ, described_as=described_as) ## adding other Aggregations Object to self.aggs
     return self
 
   def dimension_facets(self, *dimension, output_name=None, typ="String"):
     d_facets = DimensionFacet()
-    self.d_facets = d_facets(*dimension, output_name=output_name, typ=typ)
+    self._d_facets = d_facets(*dimension, output_name=output_name, typ=typ)
     return self
 
   def granularity(self, period, typ="period"):
     grant = Granularity()
-    self.grant = grant(period, typ)
+    self._grant = grant(period, typ)
     return self
 
   def location(self, location_type, level_type, id, country="AU"):
     loc = Location(country=country)
-    self.loc = loc(location_type, level_type, id)
+    self._loc = loc(location_type, level_type, id)
     return self
 
   def filter(self, filt):
     if isinstance(filt, LogicFilter):
-      self.filt = filt.to_dict()
+      self._filt = filt.to_dict()
     elif isinstance(filt, dict):
-      self.filt = filt
+      self._filt = filt
     return self
 
   def time_series_reference(self,v):
     ts = TimeSeriesReference()
-    self.ts_reference = ts(v)
+    self._ts_reference = ts(v)
     return self
 
   def dumps(self):
-    self.req.update(self.dt)
-    self.req.update(self.aggs.to_dict())
-    self.req.update(self.grant)
+    self.req.update(self._dt)
+    self.req.update(self._aggs.to_dict())
+    self.req.update(self._grant)
 
-    if self.loc is not None:
-      self.req.update(self.loc)
+    if self._loc is not None:
+      self.req.update(self._loc)
 
-    if self.ts_reference is not None:
-      self.req.update(self.ts_reference)
+    if self._ts_reference is not None:
+      self.req.update(self._ts_reference)
 
-    if self.filt is not None:
-      print("filter")
-      self.req.update(self.filt)
+    if self._filt is not None:
+      self.req.update(self._filt)
 
-    if self.d_facets is not None:
-      self.req.update(self.d_facets)
+    if self._d_facets is not None:
+      self.req.update(self._d_facets)
 
     self.json = json.dumps(self.req)
     
@@ -114,12 +113,12 @@ class BasicQuery:
     return df
 
   def clear_all(self):
-    self.dt = None
-    self.aggs = None
-    self.ts_reference = None
-    self.d_facets = None
-    self.grant = None
-    self.loc = None
-    self.filt = None
+    self._dt = None
+    self._aggs = None
+    self._ts_reference = None
+    self._d_facets = None
+    self._grant = None
+    self._loc = None
+    self._filt = None
     self.req = {}
     return self
