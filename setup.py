b46a59b46c6f71cd55af01fb11559c61f9c602fd
Richard Ellison <richard.ellison@optus.com.au>
2020-03-11 13:43:53 +1100

    Adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
   name='genomeapi',
   version='1.0',
   packages=find_packages(),
+  install_requires=['pandas','requests','configparser'],
   license='Dspark Mobility Genome',
   long_description=open('README.md').read()
 )
